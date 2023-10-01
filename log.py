142bba8012d22b2fa8278044d6795c342b019a7e
Catmechanic457 <tristan.lansdown@icloud.com>
2023-10-01 10:54:00 +0100

    Fixed encoding errors hindering some interactions

--- log.py
+++ log.py
@@ -10,6 +10,9 @@ class Logs :
     def log(self, message : str, include_timestamp : bool = True, print_in_console : bool = True) -> None :
         log = f'{f"{timestamp()} " if include_timestamp else ""}{message}'
         if print_in_console : print(log)
-        file = open(f'{self.directory}log-{strftime("%d-%m-%Y")}.txt', "a")
-        file.write(f'{log}\n')
+        file = open(f'{self.directory}log-{strftime("%d-%m-%Y")}.txt', "a", encoding="utf-8")
+        try :
+            file.write(f'{log}\n')
+        except Exception as e:
+            print(e)
         file.close()
